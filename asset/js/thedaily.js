24bfeee56ab93fbfbf1fc4dda10d7ecc69896796
Ivy Rose <hi@ivyrose.me>
2021-11-06 18:42:52 -0400

    Partial revert: Search bar UI

--- asset/js/thedaily.js
+++ asset/js/thedaily.js
@@ -5,15 +5,22 @@
             $('body').toggleClass('menu-open');
             $('#top-nav a').first().focus();
         });
+        
+        $('.search-toggle').click(function() {
+            $('#search-container').toggleClass('closed').focus();
+        });
 
         $('html').click(function(e){
             $target = $(e.target);
-            if (!$target.is("#top-nav ul, #top-nav li a, .menu-toggle")) {
+            if (!$target.is("#top-nav ul, #top-nav li a, #search-form button, #search-form input, .menu-toggle, .search-toggle")) {
                 // close navigation
                 if ($('body').hasClass('menu-open')) {
                     $('#top-nav').removeClass('open').addClass('closed');
                     $('body').removeClass('menu-open');
                 }
+                
+                // close search
+                $('#search-container').addClass('closed');
             }
         });
     });
